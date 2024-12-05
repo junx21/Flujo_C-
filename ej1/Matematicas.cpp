2def3dab248f79c168899bee60d8258a5045c951
junx21 <jxuche@myuax.com>
2024-12-05 12:40:23 +0100

    ej1

---
+++ ej1/Matematicas.cpp
@@ -0,0 +1,11 @@
+#include "Matematicas.h"
+
+namespace Matematicas {
+    int Matematicas::suma(int a, int b) {
+        return a + b;
+    }
+
+    int Matematicas::resta(int a, int b) {
+        return a - b;
+    }
+}
---
+++ ej1/Matematicas.h
@@ -0,0 +1,13 @@
+// Matematicas.h
+#ifndef MATEMATICAS_H
+#define MATEMATICAS_H
+
+namespace Matematicas {
+    class Matematicas {
+    public:
+        static int suma(int a, int b);
+        static int resta(int a, int b);
+    };
+}
+
+#endif // MATEMATICAS_H
